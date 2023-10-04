9b5d8ee78e8caea64f62d4af16c99080dad6e0e6
Borzpeek <146903515+Borzpeek@users.noreply.github.com>
2023-10-04 09:17:18 +0600

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,6 +1,7 @@
 "Task 1"
 "1.1"
 print('4','8','15','16','23','42')
+“на этом задаче мы просто использовали запетю” 
 "1.2"
 print('4')
 print('8')
@@ -8,15 +9,20 @@ print('15')
 print('16')
 print('23')
 print('42')
+“на этом задаче мы по отдельности всех написали через принт ” 
 "1.3"
 a = int(input())
 print(a+1)
 print(a+2)
+“здесь мы просто плюсуем на a на 1 и на 2” 
 "1.4"
-d_ = int(input())
-_u = int(input())
-p = int(input())
-print(d_+_u+p)
+a = int(input()) 
+b = int(input()) 
+c = int(input()) 
+q = a + b + c 
+print(q) 
+“на этом задаче мы должны найти общий сумму всех чисел мы здесь всех просто плюсуем и  вводим общию сумму “ 
+
 "1.5"
 '''
 Сначала вводим длину ребра через тип данных int 
@@ -30,9 +36,11 @@ print("Volume is =",a1**3)
 print("Total surface area is =",6*(a1**2))
 "Task 2"
 "2.1"
-d_ = int(input())
-b_1 = int(input())
-print(f"{b_1//d_}\n{b_1%d_}")
+n = int(input()) 
+k = int(input()) 
+print(n // k) 
+print(n % k)  
+“здесь мы использовали арифметические операторы // и %, // это означает деление, % это остаток ” 
 "2.2"
 c = int(input())
 c1 = c//1000
@@ -40,12 +48,18 @@ c2 = (c//100)%10
 c3 = (c%100)//10
 c4 = c%10
 print('The number in the thousands position equals', c1)
+“выводит первую цифру введенного числа” 
 print('The number in the hundreds position is', c2)
+“выводит вторую цифру введенного числа” 
 print('The digit in the tens position is equal to', c3)
+“выводит третью  цифру введенного числа” 
 print('The digit in the units position is equal to', c4)
+“выводит четвертую цифру введенного числа” 
 "2.3"
-e = int(input())
-print(e//2+e%2)
+n=int(input()) 
+print((n+1)//2) 
+“здесь мы просто плюсуем 1 на 99 и делим на 2” 
+
 "2.4"
 try:
     e = int(input("Enter the number:"))
